e36bfb9c82ed40f82e93adce44e170615f2a1241
mconde422 <mconde043@gmail.com>
2025-06-12 16:58:56 +0000

    les exos sur le DOM

---
+++ exo sur les methodes et evenements/js.js
@@ -0,0 +1,12 @@
+// exo 5a
+
+const btn =  document.querySelector('button')
+console.log(btn);
+
+// exo 5b
+
+const btn2 = document.querySelector('.btn-2')
+
+btn2.innerHTML = '9b donde!'
+
+// 5c 
